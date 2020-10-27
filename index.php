e52d13417e61b46f774fb8257b1db5acad219b6f
Махонин Игорь Владиславович <I.Makhonin@krastsvetmet.ru>
2020-10-27 15:01:48 +0700

    Update bitrix env

---
+++ index.php
@@ -0,0 +1,6 @@
+<h1>Hello from bitrix server</h1>
+<ul>
+    <li><a href="php_info.php">php_info</a></li>
+    <li><a href="bitrix_server_test.php">server_test</a></li>
+    <li><a href="restore.php">restore</a></li>
+</ul>
